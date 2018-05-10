8eb4c23f86fc9cd4c4e4f2676cbe1f4ad135105e
Nafis Shayakbarov <3344474@gmail.com>
2018-05-10 13:32:05 +0500

    Create new project for iOS

---
+++ BirthdayTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Nafis JS on 10/05/2018.
+//  Copyright © 2018 Nafis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
